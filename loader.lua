ad4b91e51c1e9a1cc88c2b73d1d1cdb3f46cbd82
zuhnosu <97843307+zuhnosu@users.noreply.github.com>
2022-05-20 23:53:04 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,10 @@
-loader
+local Link = 'https://raw.githubusercontent.com/zuhnosu/poghub-crack/main/games/%s.lua'
+local True,False = pcall(function()
+    loadstring(game:HttpGet(Link:format(tostring(game.PlaceId)), true))
+end) 
+if True then 
+    loadstring(game:HttpGet(Link:format(tostring(game.PlaceId)), true))()
+    
+else 
+    warn('Unsupported game')
+end 
